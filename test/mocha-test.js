12fa0417226220cd90b843f5fdc5ef39fdc5bc35
MichaelAChrisco <michaelachrisco@gmail.com>
2016-01-02 14:27:43 -0800

    xCow example

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -1,4 +1,7 @@
 var assert = require('assert');
+var chai = require('chai');
+var expect = chai.expect;
+
 describe('Array', function() {
 	  describe('#indexOf()', function () {
 		      it('should return -1 when the value is not present', function () {
@@ -6,4 +9,25 @@ describe('Array', function() {
 				          assert.equal(-1, [1,2,3].indexOf(0));
 					      });
 		        });
+});
+
+describe("Cow", function() {
+  describe("constructor", function() {
+    it("should have a default name", function() {
+      var cow = new Cow();
+      expect(cow.name).to.equal("Anon cow");
+    });
+
+    it("should set cow's name if provided", function() {
+      var cow = new Cow("Kate");
+      expect(cow.name).to.equal("Kate");
+    });
+  });
+
+  describe("#greets", function() {
+    it("should greet passed target", function() {
+      var greetings = (new Cow("Kate")).greets("Baby");
+      expect(greetings).to.equal("Kate greets Baby");
+    });
+  });
 });
