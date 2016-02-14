1af05b13d73f5bbf438d63db0c0f35685483b5bb
stephengtuggy <sgt@StephenGTuggy.com>
2016-02-14 14:48:02 -0800

    Initial commit of script/ubuntu/15.10/setup-prereqs.bash

---
+++ script/ubuntu/15.10/setup-prereqs.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt-get --yes install git-daemon-sysvinit gitg
+sudo apt-get --yes install nodejs
+sudo apt-get --yes install nodejs-legacy
+sudo apt-get --yes install npm mocha
