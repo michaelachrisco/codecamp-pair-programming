4a9141d085b04432a43a114894338c7c3f1a7c3c
stephengtuggy <sgt@stephengtuggy.com>
2016-08-14 09:01:39 -0700

    More on Ubuntu 16.04 script

--- script/ubuntu/16.04/setup-prereqs.sh
+++ script/ubuntu/16.04/setup-prereqs.sh
@@ -3,7 +3,6 @@
 sudo apt-get --yes install git-daemon-sysvinit gitg
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get --yes install nodejs
-refreshenv
 
 sudo npm install -g npm@3
 
